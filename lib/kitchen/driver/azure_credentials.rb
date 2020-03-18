0b3f55fd04d923ae8bf7d610cd2546662e20bbc4
Hillel Antonov <dev.antonov.hillel@dev.bwater.com>
2020-03-18 16:02:32 -0400

    Add support for microsoft managed identity
    Signed-off-by: Hillel Antonov <hantonov@gmail.com>

--- lib/kitchen/driver/azure_credentials.rb
+++ lib/kitchen/driver/azure_credentials.rb
@@ -28,7 +28,7 @@ module Kitchen
         if File.file?(config_file)
           @credentials = IniFile.load(File.expand_path(config_file))
         else
-          warn "#{CONFIG_PATH} was not found or not accessible."
+          warn "#{CONFIG_PATH} was not found or not accessible. Will use environment variables or MSI"
         end
       end
 
@@ -38,33 +38,50 @@ module Kitchen
       # @return [Object] Object that can be supplied along with all Azure client requests.
       #
       def azure_options
-        options = { tenant_id: tenant_id,
-                    client_id: client_id,
-                    client_secret: client_secret,
+        options = { tenant_id: tenant_id!,
                     subscription_id: subscription_id,
                     credentials: ::MsRest::TokenCredentials.new(token_provider),
                     active_directory_settings: ad_settings,
                     base_url: endpoint_settings.resource_manager_endpoint_url }
-
+        options[:client_id] = client_id if client_id
+        options[:client_secret] = client_secret if client_secret
         options
       end
 
       private
 
+      def credentials
+        @credentials ||= {}
+      end
+
+      def credentials_property(property)
+        credentials[subscription_id]&.[](property)
+      end
+
+      def tenant_id!
+        tenant_id || raise("Must provide tenant id. Use AZURE_TENANT_ID environment variable or set it in credentials file")
+      end
+
       def tenant_id
-        ENV["AZURE_TENANT_ID"] || @credentials[subscription_id]["tenant_id"]
+        ENV["AZURE_TENANT_ID"] || credentials_property("tenant_id")
       end
 
       def client_id
-        ENV["AZURE_CLIENT_ID"] || @credentials[subscription_id]["client_id"]
+        ENV["AZURE_CLIENT_ID"] || credentials_property("client_id")
       end
 
       def client_secret
-        ENV["AZURE_CLIENT_SECRET"] || @credentials[subscription_id]["client_secret"]
+        ENV["AZURE_CLIENT_SECRET"] || credentials_property("client_secret")
       end
 
       def token_provider
-        ::MsRestAzure::ApplicationTokenProvider.new(tenant_id, client_id, client_secret, ad_settings)
+        if client_id && client_secret
+          ::MsRestAzure::ApplicationTokenProvider.new(tenant_id, client_id, client_secret, ad_settings)
+        elsif client_id
+          ::MsRestAzure::MSITokenProvider.new(msi_id: { clientid: client_id })
+        else
+          ::MsRestAzure::MSITokenProvider.new
+        end
       end
 
       #
